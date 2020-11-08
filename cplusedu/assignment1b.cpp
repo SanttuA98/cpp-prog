80f46943728d09c0e90bb9c5461eb38486131313
Santtu Asikainen <asikainensanttu98@gmail.com>
2020-11-08 12:32:29 +0200

    Create assignment1b.cpp

---
+++ cplusedu/assignment1b.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 15;
+    int i;
+    int a=1,b=1,c=1,d;
+
+    cout << a << " " << b << " " << c << " ";
+    for(i=0;i<n-3;i++){
+        d = a + b + c;
+        cout << d << " ";
+        a = b;
+        b = c;
+        c = d;
+    }
+    return 0;
+}
