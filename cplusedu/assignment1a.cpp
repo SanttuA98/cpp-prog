456dc17f5054e63d725d6d26e43afa31a3674687
Santtu Asikainen <asikainensanttu98@gmail.com>
2020-11-08 12:44:35 +0200

    Update assignment1a.cpp

--- cplusedu/assignment1a.cpp
+++ cplusedu/assignment1a.cpp
@@ -1,3 +1,9 @@
+/*
+A program which prints the first 15 numbers of the following series:
+1 0 2 1 3 2 4 3 5...
+1 1 1 3 5 9 17...
+-1 0 -3 -2 -5 -4 -7...
+*/
 #include <iostream>
 using namespace std;
 
