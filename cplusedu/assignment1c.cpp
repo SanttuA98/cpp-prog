599bd71ec31286d2e9831a9c0158d2ee8ea119d7
Santtu Asikainen <asikainensanttu98@gmail.com>
2020-11-08 12:34:56 +0200

    Create assignment1c.cpp

---
+++ cplusedu/assignment1c.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a=-1;
+    int i;
+    for(i=1;i<8;i++){
+    cout << a << " ";
+    a++;
+    cout << a << " ";
+    a = a - 3;
+        
+    }
+    cout << a << " ";
+    
+    return 0;
+}
