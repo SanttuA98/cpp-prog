9ad33f2e336b659ea21d604d426a7635422a4774
Santtu Asikainen <asikainensanttu98@gmail.com>
2020-11-08 12:53:21 +0200

    Update assignment1d.cpp

--- cplusedu/assignment1d.cpp
+++ cplusedu/assignment1d.cpp
@@ -1,3 +1,7 @@
+/*
+A program, which receives two numbers and prints to the standard output 
+device the sum of numbers between the given numbers.
+*/
 #include<iostream>
 using namespace std;
 int main()
