dc1064e8478f916f450893fb4fe8909bda7543a2
Santtu Asikainen <asikainensanttu98@gmail.com>
2020-11-08 12:39:22 +0200

    Create assignment2b.cpp

---
+++ cplusedu/assignment2b.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct student {
+    string name;
+    unsigned int id;
+    unsigned int grade;
+};
+int main()
+{
+    int i, j;
+ struct student information [5] = {
+     {"Esko", 15, 3},
+     {"Seppo", 16, 5},
+     {"Sirpa", 17, 2},
+     {"Jaska", 18, 4},
+     {"Ulla", 19, 3}
+    };
+     struct student copy;
+
+     for(i = 1; i < 5; i++){
+       for(j = 1; j < 5; j++){
+           if(information[j - 1].grade < information[j].grade){
+               copy = information[j - 1];
+               information[j - 1] = information[j];
+               information[j] = copy;
+           }
+       } 
+     }
+       
+    cout << "Students" << endl;
+    for(i = 0; i < 5; i++){
+      cout << "Name: " << information[i].name << " ID: " << information[i].id <<" Grade: " << information[i].grade << endl;
+ 
+    }
+    return 0;
+}
